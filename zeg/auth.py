e5858b6098088c7a1b22dc91050214c77b843181
Roger Noble <roger@zegami.com>
2018-04-16 16:48:51 +0100

    User data
    Wired up simple system to record user config and prefrences

--- zeg/auth.py
+++ zeg/auth.py
@@ -1,9 +1,13 @@
 # Copyright 2018 Zegami Ltd
 
 """Auth commands."""
-import sys
+import os
+
+from appdirs import user_data_dir
 
 from colorama import Fore, Style
+
+import pkg_resources
 
 from . import (
     config,
@@ -14,3 +18,18 @@ from . import (
 def login(log, session, args):
     """Authenticate and retrieve a long lived token."""
     log.warn('Login command coming soon.')
+    user_path = _init_conf_location()
+    user_data = os.path.join(user_path, '.auth')
+
+    with open(user_data, 'w') as auth:
+        auth.write('hello world')
+
+
+def _init_conf_location():
+    """Setup the config file location."""
+    app_name = pkg_resources.require('zegami-cli')[0].project_name
+    user_data = user_data_dir(app_name, 'zegami')
+    
+    if not os.path.exists(user_data):
+        os.makedirs(user_data)
+    return user_data
