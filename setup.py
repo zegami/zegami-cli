1b0f6b23d2f864c5116f698f5eaa1e9c0a1294f7
George Sainsbury <gsainsbury86@gmail.com>
2021-11-04 20:24:14 +1030

    Fix column names from SQL starting 'MKeyView('
    Change to way of printing column headers for SQL temp file. Avoids `RMKeyView([...])` included in `str()`.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='zegami-cli',
-    version='1.5.2',
+    version='1.5.3',
     description='Command Line Interface for Zegami',
     long_description=long_description,
     long_description_content_type='text/markdown',
